b64302344d7da6fa43dbf78b4fbadcb599aa93b8
leezhu <leezhu@hnu.edu.cn>
2017-08-20 20:50:13 +0800

    数组排成最小数

---
+++ sword-offer/33-arrayToMinNumber.cpp
@@ -0,0 +1,33 @@
+/*
+ *题目描述
+ 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，
+ 打印能拼接出的所有数字中最小的一个。
+ 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+ *
+ */
+
+
+
+class Solution {
+public:
+     
+
+static bool cmp(int a,int b){	//静态，升序
+         string A="";
+         string B="";
+         A+=to_string(a);
+         A+=to_string(b);
+         B+=to_string(b);
+         B+=to_string(a);
+          
+         return A<B;
+     }
+     string PrintMinNumber(vector<int> numbers) {
+         string  answer="";
+         sort(numbers.begin(),numbers.end(),cmp);
+         for(int i=0;i<numbers.size();i++){
+             answer+=to_string(numbers[i]);
+         }
+         return answer;
+     }
+}; 
